b5913e867c7f80806ddb77403a3c12bf175dd327
joseph <80433737+ilyjoseph@users.noreply.github.com>
2021-05-31 12:16:33 -0700

    just saving..

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Terror — The Discord Bot</title>
+    <meta name="theme-color" content="#36393F">
+    <meta property="og:image:url" content="https://cdn.discordapp.com/attachments/823578665475768331/848995346923388948/output-onlinepngtools.png">
+    <meta property="og:image:secure_url" content="https://cdn.discordapp.com/attachments/823578665475768331/848995346923388948/output-onlinepngtools.png">
+    <meta name="description" content="A new, rich-feature, & multi-purpose Discord bot.">
+    <link rel="icon" type="image/jpeg" sizes="180x180" href="https://cdn.discordapp.com/attachments/835212451887841361/848638682541195294/output-onlinepngtools.png">
+    <link rel="icon" type="image/jpeg" sizes="180x180" href="https://cdn.discordapp.com/attachments/835212451887841361/848638682541195294/output-onlinepngtools.png">
+    <link rel="icon" type="image/jpeg" sizes="180x180" href="https://cdn.discordapp.com/attachments/835212451887841361/848638682541195294/output-onlinepngtools.png">
+    <link rel="icon" type="image/jpeg" sizes="180x180" href="https://cdn.discordapp.com/attachments/835212451887841361/848638682541195294/output-onlinepngtools.png">
+    <link rel="icon" type="image/jpeg" sizes="180x180" href="https://cdn.discordapp.com/attachments/835212451887841361/848638682541195294/output-onlinepngtools.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/css/ope.css">
+</head>
